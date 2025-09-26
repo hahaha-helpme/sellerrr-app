6a884c46930940ce504a92b54114e1edfb81ab7f
Fabian <support@sellerrr.com>
2025-09-26 15:41:03 +0200

    backup 20250926-154103

---
+++ static/homepage/assets/sites/5JnhgpZ8Oleffstlev0iN5/6TgbQ6i84RRXjswujbcKeHSovTU4umy17zy0eOSI0gg.5QXEPW53.mjs
@@ -0,0 +1,15 @@
+const NotFound = () => null;
+
+const __StudioMetadata__ = {
+  studioDefinition: {
+    component: { type: "function" },
+    page: {
+      path: "/404",
+      elements: {},
+      sitemap: { entries: [] }
+    }
+  }
+};
+
+export { __StudioMetadata__ };
+export default NotFound;
